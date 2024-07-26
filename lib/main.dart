564835a0120bbb9575ecde79f8850c44deb32108
fahrizalaldi85 <fahrizalaldi85@gmail.com>
2024-07-26 07:06:03 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,71 @@
 import 'package:flutter/material.dart';
+import './hal_headset.dart' as headset;
+import './hal_komputer.dart' as komputer;
+import './hal_radio.dart' as radio;
+import './hal_smartphone.dart' as hp;
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: "Tab Bar",
+    home: Home(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State createState() => _State();
+}
+
+class _State extends State<Home> with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Aplikasi Hello Flutter"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text("Daftar Elektronik"),
+        bottom: TabBar(
+          controller: controller,
+          tabs: [
+            Tab(icon: Icon(Icons.computer), text: "Komputer"),
+            Tab(icon: Icon(Icons.headset), text: "Headset"),
+            Tab(icon: Icon(Icons.radio), text: "Radio"),
+            Tab(icon: Icon(Icons.smartphone), text: "Smartphone"),
+          ],
         ),
-        body: Center(
-          child: Text("Hello Flutter"),
+      ),
+      body: TabBarView(
+        controller: controller,
+        children: [
+          komputer.HalKomputer(),
+          headset.HalHeadset(),
+          radio.HalRadio(),
+          hp.HalSmartphone(),
+        ],
+      ),
+      bottomNavigationBar: Material(
+        color: Colors.amber,
+        child: TabBar(
+          controller: controller,
+          tabs: [
+            Tab(icon: Icon(Icons.computer), text: "Komputer"),
+            Tab(icon: Icon(Icons.headset), text: "Headset"),
+            Tab(icon: Icon(Icons.radio), text: "Radio"),
+            Tab(icon: Icon(Icons.smartphone), text: "Smartphone"),
+          ],
         ),
       ),
     );
